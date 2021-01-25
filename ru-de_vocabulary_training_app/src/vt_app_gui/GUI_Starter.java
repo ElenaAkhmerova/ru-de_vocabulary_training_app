111097bd27f21c05dfa75b205f6ffe6ebdf4898f
Elena <Elena@LAPTOP-V198KNO7>
2021-01-25 23:59:35 +0100

    changes in Starter

--- ru-de_vocabulary_training_app/src/vt_app_gui/GUI_Starter.java
+++ ru-de_vocabulary_training_app/src/vt_app_gui/GUI_Starter.java
@@ -1,5 +1,13 @@
 package vt_app_gui;
 
+import javax.swing.*;
+//import Vocabulary_Training_RU-DE.src.Dictionary_Backend;
+
 public class GUI_Starter {
 
+	public static void main (String[] args) {
+		//Dictionary dictionary = Dictionary_backend.getDictionary();
+		JFrame hauptFrame = new DictFrame();
+		hauptFrame.setVisible(true);
+	}
 }
